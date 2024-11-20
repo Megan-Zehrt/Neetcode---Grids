ebe3631e7c5054b86b36693b2469b8d749e60654
Megan Zehrt <zehrtmegan@gmail.com>
2024-11-20 13:16:58 -0600

    Clone Graph 20 November Algo

---
+++ Clone_Graph.Algo.20.November/algo.js
@@ -0,0 +1,70 @@
+// 133. Clone Graph
+
+
+
+// Given a reference of a node in a connected undirected graph.
+
+// Return a deep copy (clone) of the graph.
+
+// Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+// class Node {
+//     public int val;
+//     public List<Node> neighbors;
+// }
+ 
+
+// Test case format:
+
+// For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+// An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+// The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(node) {
+    
+    if(!node) return null
+
+    const visited = new Map()
+
+    function dfs(current){
+
+        if(visited.has(current)){
+            return visited.get(current)
+        }
+
+        const clone = new _Node(current.val)
+        visited.set(current, clone)
+
+        for(const n of current.neighbors){
+            clone.neighbors.push(dfs(n))
+        }
+
+        return clone
+    }
+
+    return dfs(node)
+};
