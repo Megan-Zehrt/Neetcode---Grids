a048b2136056470e578b1f6c69bf1d31e7df6ec6
Megan Zehrt <zehrtmegan@gmail.com>
2024-12-04 19:38:36 -0600

    Redundant Connections Algo 4 December - 3rd Solution (Kanh's Algorithm)

--- Redundant_Connection.Algo.4.December/algo.js
+++ Redundant_Connection.Algo.4.December/algo.js
@@ -118,3 +118,54 @@ function findRedundantConnection(edges) {
 
     return []
 }
+
+function     findRedundantConnection(edges) {
+        
+    const n = edges.length
+    const adj = Array.from({length:n+1}, () => [])
+    const indegree = new Array(n + 1).fill(0)
+
+    for(let [u, v] of edges){
+        adj[u].push(v)
+        adj[v].push(u)
+
+        indegree[u]++
+        indegree[v]++
+    }
+
+    const q = new Queue()
+
+    for(let i = 1; i <= n; i++){
+
+        if(indegree[i] === 1){
+            q.push(i)
+        }
+    }
+
+    while(!q.isEmpty()){
+
+        const node = q.pop()
+
+        indegree[node]--
+
+        for(let nei of adj[node]){
+
+            indegree[nei]--
+
+            if(indegree[nei] === 1){
+                q.push(nei)
+            }
+        }
+    }
+
+    for(let i = edges.length-1; i >= 0; i--){
+
+        const [u, v] = edges[i]
+
+        if(indegree[u] === 2 && indegree[v] === 2){
+            return [u, v]
+        }
+    }
+
+    return []
+}
